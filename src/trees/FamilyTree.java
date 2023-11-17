47acc83e0f5662d1ae2d503d5f32440f4c0e16df
Kevin Medina <medinakevin481@gmail.com>
2023-11-17 15:01:43 -0800

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -33,6 +33,8 @@ public class FamilyTree
         {
             // Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+			children.add(childNode);
+			childNode.parent = this;
         }
         
         
@@ -40,17 +42,24 @@ public class FamilyTree
         // with the given name. Returns the node, or null if not found.
         TreeNode getNodeWithName(String targetName)
         {
-            // Does this node have the target name?
-            if (?????)
+        	 // Does this node have the target name?
+            if (name.equals(targetName))
+            {
                 return this;
-                    
+			}
+
             // No, recurse. Check all children of this node.
-            for (TreeNode child: children)
+            for (TreeNode child : children)
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
+                TreeNode result = child.getNodeWithName(targetName);
+                if (result != null)
+                {
+                    return result;
+                }
             }
-            
+
             // Not found anywhere.
             return null;
         }
@@ -66,6 +75,13 @@ public class FamilyTree
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
+
+            TreeNode currentNode = this;
+            while (currentNode.parent != null)
+            {
+                ancestors.add(currentNode.parent);
+                currentNode = currentNode.parent;
+            }
 
             return ancestors;
         }
